e148abd971a4bb815db2bcf1513012d1405074fe
Kitty Walders <kittyng89@gmail.com>
2017-09-03 11:11:38 -0500

    working on printing all employees in html

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,12 +4,11 @@ var employees = [];
 
 $(document).ready(readyNow);
 
-
 function readyNow() {
     $('#addNewEmployeeButton').on('click', addEmployee);
 };
 
-function addEmployee () {
+function addEmployee() {
     // use input to create new Employee object
     new createNewEmployee(
         $('#firstNameIn').val(),
@@ -28,15 +27,28 @@ function addEmployee () {
 };
 
 function createNewEmployee(firstNameIn, lastNameIn, idNumberIn, jobTitleIn, annualSalaryIn){
-    // create a new employee based on user input
+    // create a new Employee object based on user input
     // push new Employee object into global employees array
     this.firstName = firstNameIn;
     this.lastName = lastNameIn;
     this.idNumber = idNumberIn;
     this.jobTitle = jobTitleIn;
     this.annualSalary = annualSalaryIn;
-    employees.push(this);
-};
+    employees.push(this)
+}
 
+function calMonthlySalary(employees) {
+    for (var i = 0; i < employees.length; i++) {
+        var monthlySalary = annualSalary % 12;
+        monthlySalary.toFixed(2);
+        employees.push(monthlySalary);
+    };
 
-
+// function printAllEmployees(employees) {
+//     //print and append all Employees in the array to the table
+//     for(var i = 0; i < employees.length; i++ ){
+//         $('table').append($('<tr>', {id: 'employeeTable'}));
+//         $('table').last().this.firstName;
+//         $('table').last().lastNameIn();
+//     }
+// }
