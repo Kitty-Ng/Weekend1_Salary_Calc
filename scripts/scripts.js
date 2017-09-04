9e94bfda7fa23e993e11c8cb567a2c5e9c7ee950
Kitty Ng <kittyng89@gmail.com>
2017-09-03 23:09:42 -0500

    completed Hard Mode

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,6 +6,7 @@ $(document).ready(readyNow);
 
 function readyNow() {
     $('#addNewEmployeeButton').on('click', addEmployee);
+    $("#deleteEmployeeButton").on('click', deleteEmployee);
 };
 
 function addEmployee() {
@@ -29,27 +30,11 @@ function addEmployee() {
         $('#annualSalaryIn').val('');
 
     // once newEmployee is created, append the data to employeeTable
-        $('#employeeTable').append('<tr><td>' + newEmployee.firstName + '</td><td>' + newEmployee.lastName + '</td><td>' + newEmployee.idNumber + '</td><td>' + newEmployee.jobTitle + '</td><td>' + newEmployee.annualSalary + '</td><td>' + newEmployee.monthlySalary + '</td></tr>');
-
-    // recalculate the monthlyCost across the global employeesArray
-        // loop through global employeesArray and grab the monthlySalary -> then push into monthlyCost
-        //loop through monthlyCost and sum all values to become totalMonthlyCost
-    function calTotalMonthlyCost(){
-        var totalMonthlyCost = 0;
-        for (i = 0; i < employeesArray.length; i++){
-            var monthlySalaryArray = [];
-            monthlySalaryArray.push(employeesArray[i].monthlySalary);
-
-            for (var x = 0; x < monthlySalaryArray.length; x++){
-                totalMonthlyCost += monthlySalaryArray[x];
-            };
-        
-        }; 
-        return totalMonthlyCost;
-    };
+        $('#employeeTable').append('<tr><td>' + newEmployee.firstName + '</td><td>' + newEmployee.lastName + '</td><td>' + newEmployee.idNumber + '</td><td>' + newEmployee.jobTitle + '</td><td>' + newEmployee.annualSalary + '</td><td>' + newEmployee.monthlySalary + '</td><td><div><input id="checkBox" type="checkbox" value = "true"></div></td></tr>');
 
     // append totalMonthlyCost to #monthlyCost
     $('#monthlyCost').html('<p>'+'Total Monthly Costs: $'+calTotalMonthlyCost(employeesArray)+'</p>')
+    
 };
 
 function Employee(firstNameIn, lastNameIn, idNumberIn, jobTitleIn, annualSalaryIn){
@@ -63,3 +48,26 @@ function Employee(firstNameIn, lastNameIn, idNumberIn, jobTitleIn, annualSalaryI
      //calculate monthly salary and add into new Employee Object
     this.monthlySalary = Math.ceil((annualSalaryIn / 12) * 100) / 100;
 };
+
+function calTotalMonthlyCost(){
+    // recalculate the monthlyCost across the global employeesArray
+    // loop through global employeesArray and grab the monthlySalary -> then push into monthlyCost
+    //loop through monthlyCost and sum all values to become totalMonthlyCost
+    var totalMonthlyCost = 0;
+    for (i = 0; i < employeesArray.length; i++){
+        var monthlySalaryArray = [];
+        monthlySalaryArray.push(employeesArray[i].monthlySalary);
+
+        for (var x = 0; x < monthlySalaryArray.length; x++){
+            totalMonthlyCost += monthlySalaryArray[x];
+        };
+    
+    }; 
+    return totalMonthlyCost;
+};
+
+function deleteEmployee(){
+    //for all selected employees, delete the closest tr from DOM
+    $('td input:checked').closest('tr').remove();
+};
+
