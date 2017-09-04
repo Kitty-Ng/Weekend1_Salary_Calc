8aa8a3e6b0b24f7be7bad183ad3548144a264848
Kitty Ng <kittyng89@gmail.com>
2017-09-03 19:40:17 -0500

    working total salary calculator - base mode

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -33,6 +33,26 @@ function addEmployee() {
     // once newEmployee is created, append the data to employeeTable
         $('#employeeTable').append('<tr><td>' + newEmployee.firstName + '</td><td>' + newEmployee.lastName + '</td><td>' + newEmployee.idNumber + '</td><td>' + newEmployee.jobTitle + '</td><td>' + newEmployee.annualSalary + '</td><td>' + newEmployee.monthlySalary + '</td></tr>');
 
+    // recalculate the monthlyCost across the global employeesArray
+        // loop through global employeesArray and grab the monthlySalary -> then push into monthlyCost
+        //loop through monthlyCost and sum all values to become totalMonthlyCost
+
+    function calTotalMonthlyCost(){
+        var totalMonthlyCost = 0;
+        for (i = 0; i < employeesArray.length; i++){
+            var monthlySalaryArray = [];
+            monthlySalaryArray.push(employeesArray[i].monthlySalary);
+
+            for (var x = 0; x < monthlySalaryArray.length; x++){
+                totalMonthlyCost += monthlySalaryArray[x];
+            };
+        
+        }; 
+        return totalMonthlyCost;
+    };
+
+    // append totalMonthlyCost to #monthlyCost
+    $('#monthlyCost').html('<p>'+'Total Monthly Costs: $'+calTotalMonthlyCost(employeesArray)+'</p>')
 };
 
 function Employee(firstNameIn, lastNameIn, idNumberIn, jobTitleIn, annualSalaryIn){
